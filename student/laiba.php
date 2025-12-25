def6cd1b99e204e394b065db55da7e9bbedacd7c
Laiba Mubeen <laibamubeen678@gmail.com>
2025-12-25 10:35:13 -0800

    Create laiba.php

---
+++ student/laiba.php
@@ -0,0 +1,17 @@
+<?php
+// Simple PHP example
+
+$name = "Laiba";
+$age = 21;
+
+echo "<h1>Welcome to PHP</h1>";
+echo "<p>Name: $name</p>";
+echo "<p>Age: $age</p>";
+
+// Condition
+if ($age >= 18) {
+    echo "<p>Status: Adult</p>";
+} else {
+    echo "<p>Status: Minor</p>";
+}
+?>
