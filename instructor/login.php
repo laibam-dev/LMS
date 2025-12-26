9a44e5cd8eb3cbb31ee54a88a74253c49bf8db16
masoomfatima <masoomfatima1213@gmail.com>
2025-12-26 16:09:12 +0500

    commit

--- instructor/login.php
+++ instructor/login.php
@@ -1,2 +1,22 @@
-<?php
-echo "LOGIN PAGE WORKING";
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Instructor Login</title>
+</head>
+<body>
+
+<h2>Instructor Login</h2>
+
+<form method="POST" action="login_process.php">
+    <label>Email</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <label>Password</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
